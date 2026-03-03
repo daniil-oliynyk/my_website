d4012f6a1af26a77ca22f142551439f83245cd18
daniil-oliynyk <daniil.oliynyk@yahoo.ca>
2026-03-02 22:52:50 -0500

    change ret to data

--- src/app/actions/sendEmail.ts
+++ src/app/actions/sendEmail.ts
@@ -4,24 +4,24 @@ import { Resend } from "resend"
 
 const resend = new Resend(process.env.RESEND_API_KEY)
 
-export async function sendEmail(data: { name: string; email: string; message: string; company: string}) {
+export async function sendEmail(formData: { name: string; email: string; message: string; company: string}) {
     
-    if (data.company) return;
+    if (formData.company) return;
 
-    if (!data.name || !data.email || !data.message) {
+    if (!formData.name || !formData.email || !formData.message) {
     throw new Error("Missing required fields");
   }
 
-    const {ret, error} = await resend.emails.send({
+    const {data, error} = await resend.emails.send({
         from: 'Contact <onboarding@resend.dev>',
         to: "daniil.oliynyk@yahoo.ca",
-        subject: `New email from ${data.name}`,
-        replyTo: data.email,
+        subject: `New email from ${formData.name}`,
+        replyTo: formData.email,
         text: `
-            Name: ${data.name}
-            Email: ${data.email}    
+            Name: ${formData.name}
+            Email: ${formData.email}    
 
-            ${data.message}
+            ${formData.message}
         `,
     });
 
